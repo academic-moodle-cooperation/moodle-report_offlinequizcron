68ea5b62836ec2c031cc380946f2cbd9085dc381
Thomas Wedekind <thomas.wedekind@univie.ac.at>
2019-08-08 16:29:50 +0200

    change requirement to actual moodle version 3.7

--- version.php
+++ version.php
@@ -30,6 +30,7 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2019051400;                 // The current plugin version (Date: YYYYMMDDXX)
 $plugin->release   = "v3.7.0";      // User-friendly version number.
-$plugin->requires  = 2018051700;                 // Requires this Moodle version
+$plugin->requires  = 2019052000;                 // Requires this Moodle version
+$plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'report_offlinequizcron';   // Full name of the plugin (used for diagnostics)
 $plugin->dependencies = array('mod_offlinequiz' => 2019051100);
